22241417bdfd961d33a10c22f1d01bc30702154f
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-01-29 19:26:33 -0800

    Added a buffer window to do background tasks

--- game/main.js
+++ game/main.js
@@ -14,6 +14,7 @@ const fs = require('fs')
 const { net } = require('electron')
 const http = require('http')
 let path = require('path');
+const needle = require('needle');
 //let appPath = remote.getAppPath();
 //const net = require('electron').remote.net;
 
@@ -21,11 +22,11 @@ let path = require('path');
 
 var points = 0
 var lives = 3
-
-//var shuffle = false
+var startup = 0
+    //var shuffle = false
 
 //-----------Files-------------//
-store.set('topContent', '<!DOCTYPE html><html><body><button')
+store.set('topContent', '<!DOCTYPE html><html lang="en"><html encoding="utf-8"><body><a href="index.html"><button type="button">')
 store.set('topAContent', ' type="button" onclick="javascript:Game.correct();>')
 store.set('bottomContent', '</button></body></html>')
 
@@ -41,13 +42,13 @@ function getQuestions() {
         path: 'api.php?amount=10&category=9&difficulty=easy&type=multiple', //----------------------------------------Add Customization Here
         redirect: 'follow'
     });
-    request.on('response', (response) => {
+    request.on('response', (response, q) => {
         console.log(`STATUS: ${response.statusCode}`);
         console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
 
-        response.on('data', (chunk) => {
+        response.on('data', (chunk, q) => {
             console.log(`BODY: ${chunk}`)
-            q = chunk
+            q = response
         });
     });
     request.on('finish', () => {
@@ -64,8 +65,7 @@ function getQuestions() {
     });
     request.setHeader('Content-Type', 'application/json');
     request.end();
-    request.
-    return JSON.stringify(request.response.results)
+    return request.response.results
 }
 
 function mainWindow(action) {
@@ -82,34 +82,46 @@ function mainWindow(action) {
     } else {
         console.log("ERROR: No action passed to mainWindow(); Availible actions are ''open'' or ''close''")
     }
-
+    window.on('page-title-updated', () => {
+        start(startup, window)
+        startup++
+    })
 }
 
 function qaWindows(action) {
     // Create the browser window.
-    const window = new BrowserWindow({
+    const q = new BrowserWindow({
         width: 500,
         height: 500,
         name: "a",
         transparent: true
     })
-    const window1 = new BrowserWindow({
+    const a = new BrowserWindow({
         width: 500,
         height: 500,
         name: "b",
         transparent: true
     })
     if (action === "open") {
-        window.loadFile('q.html')
-        window1.loadFile('a.html')
+        q.loadFile('q.html')
+        a.loadFile('a.html')
     } else if (action === "close") {
-        window.close()
-        window1.close()
+        q.close()
+        a.close()
     } else {
         console.log("[ERROR] - No action passed to qaWindows(); Availible actions are ''open'' or ''close''")
     }
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+
+
+    a.on('page-title-updated', () => {
+        qaWindows('close')
+    })
+
+    q.on('page-title-updated', () => {
+            qaWindows('close')
+        })
+        // Open the DevTools.
+        // mainWindow.webContents.openDevTools()
 }
 //------------------ Game Functions ------------------//
 function game(question, questionNum, lives, points, correctA, time) {
@@ -117,7 +129,7 @@ function game(question, questionNum, lives, points, correctA, time) {
     while (lives > 0 || questionNum > 10) {
         points += 10
         qaWindows("close")
-        fs.writeFile('a.html', store.get('topContent') + store.get('topAContent') + question[questionNum].question + store.get('bottomContent'), function(err) {
+        fs.writeFile('a.html', store.get('topContent') + question[questionNum].correct_answer + store.get('bottomContent'), function(err) {
             if (err) throw err;
             console.log('Replaced a.html!');
         });
@@ -132,22 +144,35 @@ function game(question, questionNum, lives, points, correctA, time) {
     }
 }
 
-/*function setupQuestions() {
-var data = http.request("http://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
-    //("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
-console.log(data.questions)
-return data
+function setupQuestions() {
+    var data = http.request("http://api.fungenerators.com/trivia/random")
+        //("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+        //https://api.fungenerators.com/trivia/random
+    console.log(data.questions)
+
+
+
+    data = needle.get('http://api.fungenerators.com/trivia/random', { json: true }, (err, res) => {
+        if (err) {
+            return console.log(err);
+        }
+        return res.body
+    });
+
+    return data
+
 }
-*/
+
 //------------------ Elctron Methods ------------------//
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    mainWindow("open")
-    const question = getQuestions()
+
+    const question = setupQuestions()
     console.log("\n\n\n\n\n\n\n\n\n" + question.results)
+    mainWindow("open")
 });
 app.on('activate', function() {
     // On OS X it's common to re-create a window in the app when the
@@ -170,14 +195,38 @@ var time
     //console.log(question[1].type)
     // HTML => Electron Triggers
 var correctA = false
-exports.correct = () => correctA = true;
-exports.incorrect = () => correctA = false;
-exports.next = () => correctA = false;
-exports.start = () => mainWindow("close") //game(question, questionNum, lives, points, correctA, time)
-ipcMain.on('start', () => console.log('start \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n aaaaaaaaaaaa'))
-    /*ipcMain.on('start', (event, arg) => {
-        if (arg === true) {
-            console.log("Hi!")
-        } else {}
+
+
+function start(a, window) {
+    if (a >= 1) {
+        console.log("\nStarting! " + a + " \n")
+        bufferWin('open')
+        window.close()
+        game(question, questionNum, lives, points, correctA, time)
+    } else {
+        console.log("\nSkipping on startup\n")
+    }
+}
+
+function bufferWin(action) {
+    const mainWindow = new BrowserWindow({
+        height: 1,
+        width: 1,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        },
+        icon: path.join(__dirname, 'assets', 'img', 'icon.png'),
+        title: 'Trivia with Twist',
+        transparent: true
     });
-    */
+    mainWindow.setTitle('Trivia with Twist')
+    mainWindow.loadFile('index.html')
+    if (action === "open") {
+        mainWindow.loadFile('index.html')
+    } else if (action === "close") {
+        mainWindow.close()
+    } else {
+        console.log("[ERROR] - No action passed to bufferWin; Availible actions are ''open'' or ''close''")
+    }
+}
