a64f14af91582cded49e73ccca99f828f7c48630
child_duckling <19170969+child-duckling@users.noreply.github.com>
2021-12-05 13:57:45 -0800

    trivia game coming along

--- game/main.js
+++ game/main.js
@@ -1,232 +1,152 @@
 //------------------ Do not remove -----------------//
 // Modules to control application life and create native browser window
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-const { app, BrowserWindow, Menu, ipcRenderer, ipcMain, ClientRequest, electron } = require('electron')
+const { app, BrowserWindow, Menu, ipcRenderer, ipcMain, ClientRequest, electron, shell, CommandLine } = require('electron')
 const Store = require('electron-store')
-var remote = require('electron').remote
-const { promisify } = require('util')
-ipcMain.handle('lstat', async(event, filename) => await fs.promises.lstat(filename))
-const store = new Store()
-const storage = require('electron-storage')
-const jsom = require('electron-json-storage')
-const os = require('os')
-const fs = require('fs')
-const { net } = require('electron')
-const http = require('http')
-let path = require('path');
-const needle = require('needle');
-//let appPath = remote.getAppPath();
-//const net = require('electron').remote.net;
+const fetch =
+    import ('node-fetch')
+    //let appPath = remote.getAppPath();
+    //const net = require('electron').remote.net;
 
 //-----Default Settings-------------//
 
 var points = 0
 var lives = 3
-var startup = 0
     //var shuffle = false
-
-//-----------Files-------------//
-store.set('topContent', '<!DOCTYPE html><html lang="en"><html encoding="utf-8"><body><a href="index.html"><button type="button">')
-store.set('topAContent', ' type="button" onclick="javascript:Game.correct();>')
-store.set('bottomContent', '</button></body></html>')
-
-//------------------ Electron Setup -----------------//
-function getQuestions() {
-    //http://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
-    //var get = document.getElementById('get');
-    var q
-    const request = net.request({
-        method: 'GET',
-        protocol: 'http:',
-        hostname: 'opentdb.com',
-        path: 'api.php?amount=10&category=9&difficulty=easy&type=multiple', //----------------------------------------Add Customization Here
-        redirect: 'follow'
-    });
-    request.on('response', (response, q) => {
-        console.log(`STATUS: ${response.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-
-        response.on('data', (chunk, q) => {
-            console.log(`BODY: ${chunk}`)
-            q = response
-        });
-    });
-    request.on('finish', () => {
-        console.log('Request is Finished')
-    });
-    request.on('abort', () => {
-        console.log('Request is Aborted')
-    });
-    request.on('error', (error) => {
-        console.log(`ERROR: ${JSON.stringify(error)}`)
-    });
-    request.on('close', (error) => {
-        console.log('Last Transaction has occured')
-    });
-    request.setHeader('Content-Type', 'application/json');
-    request.end();
-    return request.response.results
-}
-
-function mainWindow(action) {
-    const window = new BrowserWindow({
-        width: 1000,
-        height: 1000,
-        name: "Game",
-        transparent: false
-    })
-    if (action === "open") {
-        window.loadFile('main.html')
-    } else if (action === "close") {
-        window.close()
-    } else {
-        console.log("ERROR: No action passed to mainWindow(); Availible actions are ''open'' or ''close''")
-    }
-    window.on('page-title-updated', () => {
-        start(startup, window)
-        startup++
-    })
-}
-
-function qaWindows(action) {
-    // Create the browser window.
-    const q = new BrowserWindow({
-        width: 500,
-        height: 500,
-        name: "a",
-        transparent: true
-    })
-    const a = new BrowserWindow({
-        width: 500,
-        height: 500,
-        name: "b",
-        transparent: true
-    })
-    if (action === "open") {
-        q.loadFile('q.html')
-        a.loadFile('a.html')
-    } else if (action === "close") {
-        q.close()
-        a.close()
-    } else {
-        console.log("[ERROR] - No action passed to qaWindows(); Availible actions are ''open'' or ''close''")
-    }
+var start = false
 
 
-    a.on('page-title-updated', () => {
-        qaWindows('close')
-    })
-
-    q.on('page-title-updated', () => {
-            qaWindows('close')
-        })
-        // Open the DevTools.
-        // mainWindow.webContents.openDevTools()
-}
-//------------------ Game Functions ------------------//
-function game(question, questionNum, lives, points, correctA, time) {
-
-    while (lives > 0 || questionNum > 10) {
-        points += 10
-        qaWindows("close")
-        fs.writeFile('a.html', store.get('topContent') + question[questionNum].correct_answer + store.get('bottomContent'), function(err) {
-            if (err) throw err;
-            console.log('Replaced a.html!');
-        });
-
-        fs.writeFile('q.html', store.get('topContent') + question[questionNum].question + store.get('bottomContent'), function(err) {
-            if (err) throw err;
-            console.log('Replaced q.html!');
-        });
-
-        qaWindows("open")
-        while (correctA === false) { time = time + 1 }
-    }
-}
-
-function setupQuestions() {
-    var data = http.request("http://api.fungenerators.com/trivia/random")
-        //("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
-        //https://api.fungenerators.com/trivia/random
-    console.log(data.questions)
-
-
-
-    data = needle.get('http://api.fungenerators.com/trivia/random', { json: true }, (err, res) => {
-        if (err) {
-            return console.log(err);
-        }
-        return res.body
-    });
-
-    return data
-
-}
-
-//------------------ Elctron Methods ------------------//
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-
-    const question = setupQuestions()
-    console.log("\n\n\n\n\n\n\n\n\n" + question.results)
-    mainWindow("open")
-});
-app.on('activate', function() {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow()
-    }
-})
-
-app.once('ready-to-show', () => {
-    app.show()
-})
-
-
-//------------------ Game Setup -----------------//
-
-//var question = gsetupQuestions()
-var questionNum = -1
-var time
-    //console.log(question[1].type)
-    // HTML => Electron Triggers
-var correctA = false
-
-
-function start(a, window) {
-    if (a >= 1) {
-        console.log("\nStarting! " + a + " \n")
-        bufferWin('open')
-        window.close()
-        game(question, questionNum, lives, points, correctA, time)
-    } else {
-        console.log("\nSkipping on startup\n")
-    }
-}
-
-function bufferWin(action) {
-    const mainWindow = new BrowserWindow({
-        height: 1,
-        width: 1,
+function mainWindow() {
+    const win = new BrowserWindow({
+        height: 600,
+        width: 800,
         webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true
+            nodeIntegration: true
         },
-        icon: path.join(__dirname, 'assets', 'img', 'icon.png'),
-        title: 'Trivia with Twist',
-        transparent: true
+        title: 'Trivia with a Twist',
     });
-    mainWindow.setTitle('Trivia with Twist')
-    mainWindow.loadFile('index.html')
-    if (action === "open") {
-        mainWindow.loadFile('index.html')
-    } else if (action === "close") {
-        mainWindow.close()
-    } else {
-        console.log("[ERROR] - No action passed to bufferWin; Availible actions are ''open'' or ''close''")
+
+    win.setTitle('Trivia with a Twist');
+    win.loadFile('pages/start.html');
+    //win.webContents.openDevTools();
+}
+
+app.whenReady().then(main);
+
+app.on('window-all-closed', () => {
+    //if (process.platform !== 'darwin') {
+    app.quit();
+    //d}
+});
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
     }
+});
+
+function game(trivia, qNum = 0) {
+    const { screen } = require('electron')
+    let centerOfScreenX = screen.getPrimaryDisplay().bounds.width / 2
+    let centerOfScreenY = screen.getPrimaryDisplay().bounds.height / 2
+
+    const q = new BrowserWindow({
+        x: centerOfScreenX,
+        y: (centerOfScreenY / 2) / 2,
+        height: 300,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+
+    })
+    const a1 = new BrowserWindow({
+        x: centerOfScreenX,
+        y: centerOfScreenY + 100,
+        height: 80,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    })
+    const a2 = new BrowserWindow({
+        x: centerOfScreenX,
+        y: centerOfScreenY + 200,
+        height: 80,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    })
+    const a3 = new BrowserWindow({
+        x: centerOfScreenX,
+        y: centerOfScreenY + 300,
+        height: 80,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    })
+    const a4 = new BrowserWindow({
+        x: centerOfScreenX,
+        y: centerOfScreenY + 400,
+        height: 80,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    })
+    q.loadFile(`pages/q.html`) //?q=${trivia[0].question}
+    q.webContents.executeJavaScript(`document.getElementById('question').innerHTML = "${trivia[qNum].question}"`)
+    q.title = `Question ${qNum + 1} of ${trivia.length}`
+    q.mo
+    q.show()
+
+
+    a1.loadFile(`pages/a.html`)
+    a1.webContents.executeJavaScript(`document.getElementById('answer').innerHTML = "${trivia[qNum].correct_answer}"`)
+
+    a2.loadFile(`pages/a.html`)
+    a2.webContents.executeJavaScript(`document.getElementById('answer').innerHTML = "${trivia[qNum].incorrect_answers[0]}"`)
+
+    a3.loadFile(`pages/a.html`)
+    a3.webContents.executeJavaScript(`document.getElementById('answer').innerHTML = "${trivia[qNum].incorrect_answers[1]}"`)
+
+    a4.loadFile(`pages/a.html`)
+    a4.webContents.executeJavaScript(`document.getElementById('answer').innerHTML = "${trivia[qNum].incorrect_answers[2]}"`)
+
+    a1.on('close', () => {
+        q.destroy()
+        a1.destroy()
+        a2.destroy()
+        a3.destroy()
+        a4.destroy()
+        if (qNum + 1 >= trivia.length) {
+            game(trivia, qNum + 1)
+        } else {
+            main()
+        }
+    })
+
+}
+
+
+function main() {
+
+    const m = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            nodeIntegration: true
+        },
+    })
+    m.loadFile('pages/start.html')
+    m.focus()
+
+
+    ipcMain.on('start', (e, trivia) => {
+        m.hide()
+        game(trivia)
+
+        //trivia
+
+    })
 }
--- game/renderer.js
+++ game/renderer.js
@@ -1,15 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
+
+const { ipcRenderer } = require("electron/renderer")
+
 // All of the Node.js APIs are available in this process.
+const QueryString = window.location.search;
+const urlParams = new URLSearchParams(QueryString);
 
-document.getElementById("body").style.property.backgroundColor = fff0
 
-const { ipcRenderer } = require('electron');
+async function pullDataAndSend() {
+    let trivia = await fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`)
+    trivia = await trivia.json()
+    console.log(trivia.results)
+    ipcRenderer.send('start', trivia.results)
+}
+document.getElementById('start').addEventListener('click', () => {
+    document.getElementById('start').innerHTML = `<div class="spinner-border" role="status"></div>`
+    pullDataAndSend()
+})
 
-document
-    .querySelector('start')
-    .addEventListener('click', () => {
-        ipcRenderer.send('start');
-    });
-const { remote } = require('electron');
-const main = remote.require('./main.js');
+if (urlParams.has('question')) document.getElementById("question").innerHTML = urlParams.get('question')
+if (urlParams.has('answer')) document.getElementById("answer").innerHTML = urlParams.get('answer')
